116be1722be4686333b2e9906b9b4c3a8f9c89a5
Ray Orolé <orole.ray@protonmail.com>
2023-09-12 22:05:07 +0200

    Update schema

--- app/[lang]/admin/layout.tsx
+++ app/[lang]/admin/layout.tsx
@@ -1,41 +1,37 @@
 import { Metadata } from "next";
-import Image from "next/image";
 
 import { Separator } from "@/components/ui/separator";
 import SidebarNav from "@/components/admin/sidebar-nav";
 import { Button } from "@/components/ui/button";
 import { ExitIcon, HomeIcon } from "@radix-ui/react-icons";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import Link from "next/link";
+import { authOptions } from "@/lib/auth";
 
 export const metadata: Metadata = {
-  title: "Gradely | Settings",
-  description: "Manage your profile, account, and notifications settings.",
+  title: "Gradely | Admin",
+  description: "Admin panel for your school.",
 };
 
-interface SettingsLayoutProps {
+interface AdminLayoutProps {
   children: React.ReactNode;
 }
 
-export default function SettingsLayout({ children }: SettingsLayoutProps) {
+export default async function AdminLayout({ children }: AdminLayoutProps) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/signin");
+  }
+
+  if (session?.user.role !== "ADMIN") {
+    redirect("/dash");
+  }
+
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+      <div className="space-y-6 p-10 pb-16 md:block">
         <div className="space-y-0.5 flex justify-between items-center">
           <div>
             <h2 className="text-2xl font-bold tracking-tight">Admin panel</h2>
